10e02c1281fbcb6b62035399991dd6912ce228b9
Alejandro Cortez <ziker@live.com.mx>
2021-03-07 14:10:46 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const { makeExecutableSchema } = require("graphql-tools");
+const express = require("express");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resolvers");
+
+const app = express();
+const port = process.env.port || 3000;
+
+// Initial Schema
+const typeDefs = readFileSync(
+  join(__dirname, "lib", "schema.graphql"),
+  "utf-8"
+);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: resolvers,
+    graphiql: true
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/graphql`);
+});
