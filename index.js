ca704ff4809da40f93262385bd9175910fd4d5b6
Alejandro Cortez <ziker@live.com.mx>
2021-03-07 16:05:10 -0600

    changed port to PORT

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { join } = require("path");
 const resolvers = require("./lib/resolvers");
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // Initial Schema
 const typeDefs = readFileSync(
